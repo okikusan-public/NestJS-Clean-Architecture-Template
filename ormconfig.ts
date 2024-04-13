ac4fda73fae5ecf61328ff93694eeb6cc7ec740b
okikusan_public <hiro.kiku.win.win@gmail.com>
2024-04-13 11:10:07 +0000

    Fix typeorm migration issue

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,16 +3,16 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: 5432,
+  port: parseInt(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  entities: ['dist/src/domains/**/entities/*.js'],
-  migrations: ['dist/src/infrastructure/typeorm/migrations/*.js'],
-  subscribers: ['dist/src/infrastructure/typeorm/subscribers/*.js'],
-  synchronize: false,
+  entities: ['/usr/src/app/dist/src/domains/**/entities/*.js'],
+  migrations: ['/usr/src/app/dist/src/infrastructure/typeorm/migrations/*.js'],
+  subscribers: ['/usr/src/app/dist/src/infrastructure/typeorm/subscribers/*.js'],
+  synchronize: Boolean(process.env.DB_SYNCRONIZE),
   migrationsRun: true,
-  logging: false,
+  logging: true,
 };
 
 export default config;
