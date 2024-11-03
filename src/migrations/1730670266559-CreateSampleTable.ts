dfe7c332d5506a9ea4502942cf6ba430dd6666f2
菊池裕之 <kikuchihiroyuki@kikuchihiroyukinoMacBook-Pro-2.local>
2024-11-04 06:59:48 +0900

    Add structure to generate migration

---
+++ src/migrations/1730670266559-CreateSampleTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateSampleTable1730670266559 implements MigrationInterface {
+    name = 'CreateSampleTable1730670266559'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "sample_entity" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, CONSTRAINT "PK_e873152a04c344da778041e482c" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "sample_entity"`);
+    }
+
+}
